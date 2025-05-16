50588ed0dad172ac3a988932bcd44035898e1515
foysalpranto121 <foysalpranto2002@gmail.com>
2025-05-16 15:43:54 +0600

    Add new files

---
+++ Datatype.py
@@ -0,0 +1,41 @@
+kodu = " hello jadu"
+print(kodu)
+# number data type {int float complex}
+# int type
+a = 10
+print(a)
+print(type(a))
+# float type
+b = 10.5
+print(b)
+print(type(b))
+# complex type
+c = 10 + 5j
+print(c)
+print(type(c))
+# string data type ( text type data)
+name="kodu"
+print(name)
+print(type(name))
+another_name="kasem"
+print( name + another_name)
+print( name + " " + another_name)
+print( "your name is " +  name )
+# boolean data type (true or false)
+Bool= True
+print(Bool)
+print(type(Bool))
+x=10
+y=20
+print(x>y)
+print(x<y)
+print(x==y)
+print(x!=y)
+
+
+
+
+
+
+
+
