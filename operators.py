e0fa7d15fa511502ffe8fd62ae3b6c52e462990d
foysalpranto121 <foysalpranto2002@gmail.com>
2025-05-16 21:47:09 +0600

    Add new files

---
+++ operators.py
@@ -0,0 +1,34 @@
+#operator
+# arithmatic operator
+a=10
+b=20
+print(a+b) #addition
+print(b-a) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a%b) #modulus
+print(a**b) #exponent 10 to the power 20
+print(a//b) #floor division ( rounds the result down to the nearest integer)
+
+#assignment operator
+a=10
+b=20
+a+=b #a=a+b
+print(a)
+a-=b #a=a-b
+print(a)
+a*=b #a=a*b
+print(a)
+a/=b #a=a/b
+print(a)
+a%=b #a=a%b
+print(a)
+a**=b #a=a**b
+print(a)
+a//=b #a=a//b
+print(a)
+
+
+
+
+
