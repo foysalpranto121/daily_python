91e6afcf65d8220561fd9d0c2c3554894941a77b
foysalpranto121 <foysalpranto2002@gmail.com>
2025-05-16 13:30:53 +0600

    Add new files

---
+++ day2.py
@@ -0,0 +1,33 @@
+#print diye amra kno kisu print kori
+
+# comment diye amra kno kisu code er kaj korano off kore dei
+# comment er jonno # use korte hoy
+
+print("Hello, World!")
+
+#module and pip
+#module hocche python er code er collection
+#pip hocche python er package manager jar maddhome amra python er module install korte pari
+
+#python variables
+#variables er jonno $ use korte hoy
+#python er variable er jonno $ use korte hoy na
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#python data types
+#string
+#integer
+#float
+#boolean
+#list
+#tuple
+#dictionary
+#set
+
+name ="pranto"
+print(name)
+
