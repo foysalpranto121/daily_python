92c0f5b814feb54e5752f238f24eac13c8bb76ca
foysalpranto121 <foysalpranto2002@gmail.com>
2025-05-16 15:55:59 +0600

    Add new files

---
+++ format.py
@@ -0,0 +1,16 @@
+num1=20
+num2=30
+sum=num1+num2
+print("The sum is ",sum)
+print(f"The sum of {num1} and {num2} is {sum}")
+print(f"sum is {num1+num2}")
+#use f keyword to format
+#use + to concatenate
+#use , to separate
+#use \n to newline
+#use {} to format
+userName ="pranto"
+print(f"my name is {userName} & sum is {sum}")
+
+
+
