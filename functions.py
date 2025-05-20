56122a1210e3edc9f28aaed1876979c821ec4c2b
foysalpranto121 <foysalpranto2002@gmail.com>
2025-05-20 23:19:36 +0600

    Add new files

---
+++ functions.py
@@ -0,0 +1,26 @@
+#types of functions
+#1. built in functions
+#2. user defined functions
+#3. lambda functions
+#4 recursive functions
+
+#user defined functions
+#define def keyword
+
+#syntax
+# def functionName():
+#     code
+#     return
+def myfunction( a,b):  # parameters
+    sum =a+b
+    print(sum)
+    # sub=a-b
+    #
+    # print(sub)
+myfunction(10,20) #pasing arguments and passing parameters value
+myfunction(20,30)
+
+def newFunction(x,y,z):
+    multi=x*y*z
+    print(multi)
+newFunction(11,16,11)
