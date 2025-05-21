4955cbc63d59106e57bc248dcb229613a139c342
foysalpranto121 <foysalpranto2002@gmail.com>
2025-05-22 03:08:18 +0600

    Add new files

---
+++ Module.py
@@ -0,0 +1,15 @@
+def pranto():
+    print("hello")
+
+        
+        
+rahim={
+
+       "school":"dhanmondi",
+       "desh":"RASS",
+       "home":"dhanmondi",
+       "car":"bmw",
+       "tk":"100 core"
+
+
+   }
---
+++ acess module.py
@@ -0,0 +1,10 @@
+import Module
+Module.pranto()
+#or
+import Module as m
+m.pranto()
+print(Module.rahim["home"])
+
+#or
+from Module import pranto #for specific module
+pranto()
