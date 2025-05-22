cb4f319f469a2ebf61c8889d8b904de936ccf13b
foysalpranto121 <foysalpranto2002@gmail.com>
2025-05-22 22:05:31 +0600

    Add new files

--- OOB.py
+++ OOB.py
@@ -72,4 +72,24 @@ print(k.BrokenHome)
 
 ## hybrid and hierarchical inheritance
 
+#oop
+#object oriented programming
+#encapsulation
+#constructor
+#parameteraise constructor
+#Non parameteraise constructor
+# class parrentInfo:
+#     def  PantoFamily(self,name, age):
+#
+#         print( f"My name is {self.name} and my age  is {self.age}")
+#
+# p1=parrentInfo()
+#
+# p1.PantoFamily(20)
+
+# parameteraise constructor
+class studentInfo:
+    def __init__(self,roll,school):
+         print(f"My roll is {roll} and my age  is {school}")
+p1=studentInfo(21,"dhanmondi")
 
